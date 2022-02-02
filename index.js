7de7e2c9c02fa9a8c38f6609dacbc3073648af98
Teyddie22 <teyddie@gmail.com>
2022-02-02 06:33:34 +0300

    nav movement

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav__links');
+const navLinks = document.querySelectorAll('.nav__links li');
+
+const navSlider = function () {
+	burger.addEventListener('click', () => {
+		nav.classList.toggle('nav-active');
+
+		navLinks.forEach((el, i) => {
+			if (el.style.animation) {
+				el.style.animation = '';
+			} else {
+				el.style.animation = `navLinksMovement 0.5s ease forwards ${
+					i / 7 + 0
+				}s`;
+			}
+		});
+	});
+};
+
+navSlider();
