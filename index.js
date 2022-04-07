56ef6a5be183051bca0aa607d6c52166c6e41b58
Teyddie22 <teyddie@gmail.com>
2022-04-07 07:41:37 +0300

    Scroll navigation

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav__links');
 const navLinks = document.querySelectorAll('.nav__links li');
+const header = document.querySelector('.header');
 
+//? NAV
+
+//! Nav slide section
 const navSlider = function () {
 	burger.addEventListener('click', () => {
 		nav.classList.toggle('nav-active');
@@ -23,3 +27,23 @@ const navSlider = function () {
 };
 
 navSlider();
+
+//! Nav selection EVENT
+
+const navScrollBehavior = function (e) {
+	e.preventDefault();
+
+	const navLinks = e.target
+		.closest('.navigation')
+		.querySelectorAll('.nav__link');
+
+	if (navLinks) {
+		const id = e.target.getAttribute('href');
+		console.log(id);
+		document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+	}
+};
+
+//! ADDEVENTLISTENERS
+
+nav.addEventListener('click', navScrollBehavior);
